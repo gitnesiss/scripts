4f8bfa3a410baa2a8a668e0eed621ed6aed3d8d8
genesiss <s21genesiss@gmail.com>
2023-01-31 10:45:23 +0700

    main: 001 commit. Add first scripts.

---
+++ install_develop_prog_and_libs_on_ubuntu.sh
@@ -0,0 +1,77 @@
+# Copyright 17.01.2023  s21genesiss@gmail.com
+# Version 0.3
+# Скрипт для установки программ для разработки.
+
+#!/bin/bash
+
+# Цвет текста
+# 0 - Нормальный
+# 1 - Жирный текст
+# 2 - Тусклый текст
+# 3 - Курсив
+# 4 - Подчеркнутый текст
+BLACK="\e[30m"
+RED="\e[31m"
+GREEN="\e[32m"
+BOLDGREEN="\e[1;32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+PURPLE="\e[35m"
+TURQUOISE="\e[36m"
+GREY="\e[37m"
+ENDCOLOR="\e[0m"
+
+# Цвет фона
+BLACKBACK="\e[40m"
+REDBACK="\e[41m"
+GREENBACK="\e[42m"
+YELLOWBACK="\e[43m"
+BLUEBACK="\e[44m"
+PURPLEBACK="\e[45m"
+TURQUOISEBACK="\e[46m"
+GREYBACK="\e[47m"
+
+
+USER=$(whoami)
+
+echo -e "${GREEN}Hello \e[1;${GREEN}$USER.${ENDCOLOR}"
+
+echo -e "${BLUE}Обновление системы.${ENDCOLOR}"
+sudo apt update & upgrade -y
+echo -e "${BLUE}Установка gcc make perl.${ENDCOLOR}"
+sudo apt install gcc make perl -y
+echo -e "${BLUE}Установка отладчика GNU и коллекции компиляторов.${ENDCOLOR}"
+sudo apt install build-essential -y
+echo -e "${BLUE}Установка cmake.${ENDCOLOR}"
+sudo apt install cmake -y
+sudo apt install cmake-qt-gui -y
+echo -e "${BLUE}Установка valgrind.${ENDCOLOR}"
+sudo apt install valgrind -y
+echo -e "${BLUE}Установка OpenGL.${ENDCOLOR}"
+sudo apt-get install mesa-common-dev libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev -y
+echo -e "${BLUE}Версия OpenGL:${ENDCOLOR}"
+glxinfo | grep "OpenGL core profile version string"
+echo -e "${BLUE}Установка утилит для OpenGL.${ENDCOLOR}"
+sudo apt install mesa-utils -y
+echo -e "${BLUE}Установка texinfo.${ENDCOLOR}"
+sudo apt-get install texinfo -y
+echo -e "${BLUE}Установка библиотеки GLFW${ENDCOLOR}"
+sudo apt install libglfw3 -y
+sudo apt install libglfw3-dev -y
+echo -e "${BLUE}Установка xdg-utils для установки программы, которая открывает файлы или ссылки в предпочтительной программе.${ENDCOLOR}"
+sudo apt install xdg-utils
+echo -e "${BLUE}Установка инструмент для форматирования кода С/С++ clang-format-11.${ENDCOLOR}"
+sudo apt-get install clang-format-11 -y
+echo -e "${BLUE}Установка Qt6.02.${ENDCOLOR}"
+sudo apt update -y
+sudo apt install qtcreator -y
+echo -e "${BLUE}Установка примеров программ для Qt.${ENDCOLOR}"
+sudo apt install qtbase5-examples qtdeclarative5-examples qtbase5-doc-html -y
+echo -e "${BLUE}Установка инструментов разработки для Qt.${ENDCOLOR}"
+sudo apt install qtbase5-dev qtwayland5-dev-tools qtwayland5 -y
+echo -e "${BLUE}Установить пакеты mesa-common-dev, libgl1-mesa-dev и libglu1. -mesa-dev.${ENDCOLOR}"
+sudo apt install libglfw3-dev libglm-dev libepoxy-dev libboost-all-dev libglew-dev libopenal-dev libalut-dev -y
+echo -e "${BLUE}Завершение установки.${ENDCOLOR}"
+
+#echo "Удаление Qt Creator."
+#sudo apt-get remove qt5-default qtcreator
---
+++ watch_memory_ubuntu.sh
@@ -0,0 +1,53 @@
+# Copyright 31.01.2023  s21genesiss@gmail.com
+# Version 1.0
+# Скрипт для проверки памяти в ОС Ubuntu.
+
+#!/bin/bash
+echo -e "        \033[1;37mИнформация о системе.\033[0m"
+echo
+echo "Открыть программу которая покажет используемые ресурсы ОС, можно с помощью команды:"
+echo -e "    \033[36mgnome-system-monitor\033[0m"
+echo "Для запуска просмотра ресурсов оперативной памяти с обновлением каждую секунду:"
+echo -e "    \033[36mwatch -n 1 free -m\033[0m"
+echo "Для запуска просмотра ресурсов оперативной памяти с обновлением каждую секунду, подсвечиванием изменений (запускать из места расположения скрипта)"
+echo -e "    \033[36mwatch -t -c -d -n 1 ./memory_ubuntu.sh\033[0m"
+echo
+
+
+HOSTNAME=$(hostname)
+TIMEZONE="$(cat /etc/timezone) $(date +"%Z" -u) $(date +"%:::z")"
+USER=$(whoami)
+OS=$(uname)
+DATE=$(date +"%d %B %Y %X")
+UPTIME=$(uptime -p)
+UPTIME_SEC=$(cat /proc/uptime | awk '{print $1}')
+IP=$(hostname -I)
+GATEWAY=$(ip r | grep default | awk '{print $3}')
+RAM_TOTAL=$(free | grep -e Mem: -e Память: | awk '{printf "%.3f GB", $2/(1024. * 1024.)}')
+RAM_USED=$(free | grep -e Mem: -e Память: | awk '{printf "%.3f GB", $3/(1024. * 1024.)}')
+RAM_FREE=$(free | grep -e Mem: -e Память: | awk '{printf "%.3f GB", $4/(1024. * 1024.)}')
+SPACE_ROOT=$(df / | grep dev | awk '{printf "%.2f MB", $2/(1024.)}')
+SPACE_ROOT_USED=$(df / | grep dev | awk '{printf "%.2f MB", $3/(1024.)}')
+SPACE_ROOT_FREE=$(df / | grep dev | awk '{printf "%.2f MB", $4/(1024.)}')
+
+str01="HOSTNAME = $HOSTNAME"
+str02="TIMEZONE = $TIMEZONE"
+str03="USER = $USER"
+str04="OS = $OS"
+str05="DATE = $DATE"
+str06="UPTIME = $UPTIME"
+str07="UPTIME_SEC = $UPTIME_SEC"
+str08="IP = $IP"
+str09="GATEWAY = $GATEWAY"
+str10="RAM_TOTAL = $RAM_TOTAL"
+str11="RAM_USED = $RAM_USED"
+str12="RAM_FREE = $RAM_FREE"
+str13="SPACE_ROOT = $SPACE_ROOT"
+str14="SPACE_ROOT_USED = $SPACE_ROOT_USED"
+str15="SPACE_ROOT_FREE = $SPACE_ROOT_FREE"
+
+for arg in "$str01" "$str02" "$str03" "$str04" "$str05" "$str06" "$str07"\
+  "$str08" "$str09" "$str10" "$str11" "$str12" "$str13" "$str14" "$str15"
+do
+  echo $arg
+done
