6231ddcc9ebfe0c00e7769bae2114e88119be0e3
gitnesiss <gitnesiss@gmail.com>
2024-06-13 14:53:27 +0700

    main: 013 commit. Add commands in install_common_program_on_ubuntu.sh

--- install_common_program_on_ubuntu.sh
+++ install_common_program_on_ubuntu.sh
@@ -3,13 +3,34 @@
 # Copyright 20.04.2023  s21genesiss@gmail.com
 # Version 1.0
 # Скрипт для установки базовых программ для ubuntu
+# Изменение 13.06.2024
+
+USERNAME=$(whoami)
+SYS_NAME=$(uname)
 
 # 1. Chrome
-# Загрузить установщик репозитория стабильной версии Chrome в Ubuntu
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# Установка пакета с помощью dpkg
-sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
+# Для установки Google Chrome вам нужно будет загрузить ключ репозитория, добавить сам репозиторий и обновить список пакетов: 
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update -y
+# Установка google Chrome
+sudo apt-get install google-chrome-stable
 
 # 2. GIT
 # Установка git
 sudo apt install git -y
+# Настройка логина и электронной почты при условии что они указаны в качестве аргумента при запуске скрипта
+# -z   строка пустая, т.е. имеет нулевую длину
+# -n   строка не пустая
+if [[ -n $1 ]]
+then
+    git config --global user.name "$1"
+    git config --global user.email "$1@gmail.com"
+fi
+
+# 3. Генерация ключей для соединения с git.
+# Генерация SSH ключа в формате ed25519 
+ssh-keygen -t ed25519
+# Вывод на экран ключа для возможности его копирования и просмотра.
+cat /home/$USERNAME/.ssh/id_ed25519.pub
+echo "Вставьте выше выведенную строку в настройки доступа к github."
